e8a8de929b9b1a39800546ba002cba3fadd3ce7f
Gerson-jr <junioremerique@live.com>
2023-09-26 10:40:20 -0300

    simplificação

--- static/script.js
+++ static/script.js
@@ -1,13 +1,13 @@
+// Adicionar eventos aos botões
+document.getElementById("botao-youtube-downloader").addEventListener("click", abrirLinkYoutubeDownloader);
+document.getElementById("botao-conversor-mp4-para-mp3").addEventListener("click", abrirLinkConversorMp4ParaMp3);
+
 // Função para abrir o link do YouTube Downloader
 function abrirLinkYoutubeDownloader() {
-	window.open('/youtube-downloader');
+    window.open('/youtube-downloader');
 }
 
 // Função para abrir o link do Conversor MP4 para MP3
 function abrirLinkConversorMp4ParaMp3() {
-	window.open('/conversor-mp4-para-mp3');
+    window.open('/conversor-mp4-para-mp3');
 }
-
-// Adicionar eventos aos botões
-document.getElementById("botao-youtube-downloader").addEventListener("click", abrirLinkYoutubeDownloader);
-document.getElementById("botao-conversor-mp4-para-mp3").addEventListener("click", abrirLinkConversorMp4ParaMp3);
